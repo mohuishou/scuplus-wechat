386c394e12fc4a5069a6dbed1a9eb6dcfd0a6e28
e <1@lailin.xyz>
2018-01-12 22:45:04 +0800

    UPDATE

---
+++ src/util/http.js
@@ -0,0 +1,56 @@
+let domain = "http://localhost"
+
+let token = wx.getStorageSync("token");
+
+
+//GET请求
+function GET(requestHandler) {
+  if (typeof requestHandler === 'string') {
+    requestHandler = {
+      url: String(requestHandler),
+      params: {}
+    }
+  }
+  return request('GET', requestHandler)
+}
+
+//POST请求
+function POST(requestHandler) {
+  return request('POST', requestHandler)
+}
+
+function request(method, requestHandler) {
+  let header = {
+    "Authorization": "Bearer " + wx.getStorageSync("token")
+  }
+  if (method == "POST") {
+    header["content-type"] = "application/x-www-form-urlencoded; charset=UTF-8"
+  }
+  wx.showLoading && wx.showLoading({
+    title: '加载中...'
+  })
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: domain + requestHandler.url,
+      data: requestHandler.params,
+      method: method, // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      header: header, // 设置请求的 header
+      success: function (res) {
+        //解密
+        resolve(res.data)
+      },
+      fail: function () {
+        reject('Network request failed')
+      },
+      complete: function () {
+        wx.hideLoading && wx.hideLoading()
+      }
+    })
+  })
+
+}
+
+module.exports = {
+  get: GET,
+  post: POST
+}
