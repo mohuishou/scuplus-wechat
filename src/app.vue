48dbfbef1079c4e4b636a34e1b33987dec3eedb6
mohuishou <1@lailin.xyz>
2018-03-29 11:21:06 +0800

    新增更新检查

--- src/app.vue
+++ src/app.vue
@@ -70,6 +70,28 @@
     onLaunch() {
       this.getToken()
     }
+    onShow() {
+      const updateManager = wx.getUpdateManager()
+      updateManager.onCheckForUpdate(function(res) {
+        // 请求完新版本信息的回调
+        console.log(res.hasUpdate)
+      })
+      updateManager.onUpdateReady(function() {
+        wx.showModal({
+          title: '更新提示',
+          content: '新版本已经准备好，请重启应用',
+          showCancel: false,
+          success: function(res) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        })
+      })
+      updateManager.onUpdateFailed(function() {
+        // 新的版本下载失败
+        console.log("新版本下载失败！");
+      })
+    }
     // 微信登录
     wxLogin() {
       return new Promise((resolve, reject) => {
