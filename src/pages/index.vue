570313e65a1ee94f4aa49c58aa23e588a47af3f7
mohuishou <1@lailin.xyz>
2018-04-03 20:20:22 +0800

    修复有考试情况的报错

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,74 +1,74 @@
 <style lang="less">
-  @import "./src/less/config.less";
-  page {
-    background: @bg-color;
-  }
-  .func {
-    background: #fff;
-    margin: 20rpx;
-    margin-top: -40rpx;
-    position: relative;
-    border-radius: 10rpx;
-    box-shadow: 0rpx 4rpx 10rpx #ddd;
-    z-index: 999;
-    .func-row {
-      display: flex;
-      justify-content: space-around;
-      flex-wrap: wrap;
-      .login {
-        &.no-verify {
-          .iconfont {
-            background: #ccc !important;
-          }
+@import "./src/less/config.less";
+page {
+  background: @bg-color;
+}
+.func {
+  background: #fff;
+  margin: 20rpx;
+  margin-top: -40rpx;
+  position: relative;
+  border-radius: 10rpx;
+  box-shadow: 0rpx 4rpx 10rpx #ddd;
+  z-index: 999;
+  .func-row {
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    .login {
+      &.no-verify {
+        .iconfont {
+          background: #ccc !important;
         }
       }
     }
   }
-  .mview {
-    padding-top: 30rpx;
-    width: 20%;
-    // border-bottom: 1px solid #eee;
+}
+.mview {
+  padding-top: 30rpx;
+  width: 20%;
+  // border-bottom: 1px solid #eee;
+}
+.icon-btn {
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  align-content: space-between;
+  flex-wrap: wrap;
+  .icon {
+    @size: 80rpx;
+    width: @size;
+    height: @size;
   }
-  .icon-btn {
-    text-align: center;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    align-content: space-between;
-    flex-wrap: wrap;
-    .icon {
-      @size: 80rpx;
-      width: @size;
-      height: @size;
-    }
-    text {
-      width: 100%;
-      display: block;
-      margin-top: 2rpx;
-      font-size: 28rpx;
-    }
-  }
-  .swiper {
+  text {
     width: 100%;
-    z-index: 99;
-    top: 0;
-    swiper-item {
-      width: 100%;
-    }
-    image {
-      z-index: 30;
-      width: 100%;
-    }
+    display: block;
+    margin-top: 2rpx;
+    font-size: 28rpx;
   }
-  .ecard {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: @base-color;
-    font-size: 70rpx;
-    height: 150rpx;
-    border-bottom: 2rpx dashed #fff;
+}
+.swiper {
+  width: 100%;
+  z-index: 99;
+  top: 0;
+  swiper-item {
+    width: 100%;
   }
+  image {
+    z-index: 30;
+    width: 100%;
+  }
+}
+.ecard {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: @base-color;
+  font-size: 70rpx;
+  height: 150rpx;
+  border-bottom: 2rpx dashed #fff;
+}
 </style>
 
 <template>
@@ -109,6 +109,11 @@
           </view>
         </block>
       </schedule-card>
+      <ecard :isShow.sync="ecardBalance" footText="查看交易流水" icon="card" bg="card-ecard.png" title="一卡通余额" url="/pages/ecard" iconBg="#b92c18">
+        <view class="ecard" slot="content">
+          {{ecardBalance[0]}}
+        </view>
+      </ecard>
       <book-card iconBg="#ab96c5" icon="book" bg="card-library.png" title="我的借阅" :isShow.sync="loanBooks" footText="查看所有借阅" url="/pages/library/loan" noneText="无借阅书籍">
         <block slot="content" wx:for="{{loanBooks}}" wx:key="{{index}}">
           <view wx:if="{{item.title && index < 3}}" class="card-list">
@@ -121,11 +126,7 @@
           </view>
         </block>
       </book-card>
-      <ecard :isShow.sync="ecardBalance" footText="查看交易流水" icon="card" bg="card-ecard.png" title="一卡通余额" url="/pages/ecard" iconBg="#b92c18">
-        <view class="ecard" slot="content">
-          {{ecardBalance[0]}}
-        </view>
-      </ecard>
+
       <exam-card iconBg="#2578b5" bg="card-exam.png" icon="exam" title="考表" :isShow.sync="exams" footText="查看完整考表" url="/pages/exam" noneText="近期没有考试">
         <block slot="content" wx:for="{{exams}}" wx:key="{{index}}">
           <view class="card-list">
@@ -145,114 +146,117 @@
 </template>
 
 <script>
-  import wepy from 'wepy'
-  import HttpMixin from "../mixins/http";
-  import index from '../util/index/index'
-  import MView from '../components/mview'
-  import Card from '../components/card'
-  import Empty from '../components/empty'
-  import db from "../util/db"
-  export default class Index extends wepy.page {
-    config = {};
-    components = {
-      "mview": MView,
-      "schedule-card": Card,
-      "book-card": Card,
-      "ecard": Card,
-      "exam-card": Card,
-      "empty": Empty
-    }
-    mixins = [HttpMixin]
-    data = {
-      notices: [{
-        cover: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+import wepy from "wepy";
+import HttpMixin from "../mixins/http";
+import index from "../util/index/index";
+import MView from "../components/mview";
+import Card from "../components/card";
+import Empty from "../components/empty";
+import db from "../util/db";
+export default class Index extends wepy.page {
+  config = {};
+  components = {
+    mview: MView,
+    "schedule-card": Card,
+    "book-card": Card,
+    ecard: Card,
+    "exam-card": Card,
+    empty: Empty
+  };
+  mixins = [HttpMixin];
+  data = {
+    notices: [
+      {
+        cover:
+          "http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg",
         id: 1
-      }],
-      funcs: index.funcs,
-      swiper_height: 200,
-    };
-    computed = {
-      verify() {
-        return db.Get('verify')
-      },
-      ecardBalance() {
-        const trans = db.Get("trans")
-        if (trans.length > 0) {
-          return [trans[0].balance]
-        } else {
-          return []
-        }
-      },
-      exams() {
-        let arr = db.Get("exams")
-        let data = []
-        for (let i = 0; i < arr.length; i++) {
-          if (arr[i].t >= 0) data.push(e)
-        }
-        return data
-      },
-      loanBooks() {
-        return db.Get("loan_now")
-      },
-      todaySchedules() {
-        let schedules = db.Get('schedules')
-        if (!schedules) {
-          return
-        }
-        const todaySchedule = []
-        let today = new Date().getDay() || 7
-        schedules[today].forEach(e => {
-          if (e.course_name) {
-            todaySchedule.push(e)
-          }
-        })
-        return todaySchedule
       }
-    }
-    navigate(item) {
-      let url = item.url
-      if (item.type == 'login' && this.verify == 0) {
-        wepy.showModal({
-          title: '账号信息错误',
-          content: '统一身份认证账号未绑定或密码错误！是否前往绑定？',
-          success: function(res) {
-            if (res.confirm) {
-              wepy.navigateTo({
-                url: 'bind'
-              })
-            }
-          }
-        })
-        return
+    ],
+    funcs: index.funcs,
+    swiper_height: 200
+  };
+  computed = {
+    verify() {
+      return db.Get("verify");
+    },
+    ecardBalance() {
+      const trans = db.Get("trans");
+      if (trans.length > 0) {
+        return [trans[0].balance];
       } else {
-        wepy.navigateTo({
-          url: url
-        })
+        return [];
       }
-    }
-    methods = {
-      noticeTo(id) {
-        wepy.navigateTo({
-          url: `details?id=${id}&&from=notice`
-        })
-      },
-      to(item) {
-        this.navigate(item)
+    },
+    exams() {
+      let arr = db.Get("exams");
+      let data = [];
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i].t >= 0) data.push(arr[i]);
       }
-    };
-    async getNotice() {
-      const resp = await this.GET('/notices', {}, false)
-      this.notices = resp.data
-      this.$apply()
+      return data;
+    },
+    loanBooks() {
+      return db.Get("loan_now");
+    },
+    todaySchedules() {
+      let schedules = db.Get("schedules");
+      if (!schedules) {
+        return;
+      }
+      const todaySchedule = [];
+      let today = new Date().getDay() || 7;
+      schedules[today].forEach(e => {
+        if (e.course_name) {
+          todaySchedule.push(e);
+        }
+      });
+      return todaySchedule;
     }
-    onLoad() {
-      // 设置swipe高度 2:1
-      this.swiper_height = wepy.getSystemInfoSync().windowWidth / 2
-      this.getNotice()
-      // this.notices = []
-    }
-    onShareAppMessage(options) {
-      return {}
+  };
+  navigate(item) {
+    let url = item.url;
+    if (item.type == "login" && this.verify == 0) {
+      wepy.showModal({
+        title: "账号信息错误",
+        content: "统一身份认证账号未绑定或密码错误！是否前往绑定？",
+        success: function(res) {
+          if (res.confirm) {
+            wepy.navigateTo({
+              url: "bind"
+            });
+          }
+        }
+      });
+      return;
+    } else {
+      wepy.navigateTo({
+        url: url
+      });
     }
   }
+  methods = {
+    noticeTo(id) {
+      wepy.navigateTo({
+        url: `details?id=${id}&&from=notice`
+      });
+    },
+    to(item) {
+      this.navigate(item);
+    }
+  };
+  async getNotice() {
+    const resp = await this.GET("/notices", {}, false);
+    this.notices = resp.data;
+    this.$apply();
+  }
+  onLoad() {
+    // 设置swipe高度 2:1
+    this.swiper_height = wepy.getSystemInfoSync().windowWidth / 2;
+    this.getNotice();
+    // this.notices = []
+  }
+  onShareAppMessage(options) {
+    return {};
+  }
+}
 </script>
