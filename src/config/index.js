2de4ab007890c4c8dbb7ef4b4af0c0ba3a80260c
mohuishou <1@lailin.xyz>
2018-04-05 14:42:37 +0800

    update

--- src/config/index.js
+++
@@ -1,7 +0,0 @@
-const env = process.env.NODE_ENV === 'production' ? 'prod' : 'dev'
-const domains = {
-  dev: 'http://localhost:8880',
-  prod: 'https://api.scuplus.cn'
-}
-export const domain = domains[env]
-export const version = '0.7.0'
