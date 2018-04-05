5fab2cb02e37e1fc2fa9e88381890e7e67b77bdb
mohuishou <1@lailin.xyz>
2018-04-05 15:03:25 +0800

    个人中心新增清空缓存，用户qq群复制

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -3,6 +3,12 @@
   page {
     background: @bg-color;
   }
+  .warn {
+    .list {
+      background: #ee3f4d !important;
+      color: #fff;
+    }
+  }
   .lists {
     .list {
       padding: 10rpx 20rpx;
@@ -51,6 +57,12 @@
           </view>
         </mview>
       </repeat>
+      <mview2 class="warn" @mtap.user="clear">
+        <view slot="content" class="list">
+          <view class="name">清空缓存</view>
+          <view class="iconfont icon-arrow-right"></view>
+        </view>
+      </mview2>
     </view>
   </view>
 </template>
@@ -60,30 +72,41 @@
   import MView from "components/mview";
   import HttpMixin from "mixins/http";
   import db from "util/db";
+  import ToastMixin from "mixins/toast";
   export default class My extends wepy.page {
     config = {
       navigationBarTitleText: "个人中心"
     };
     components = {
-      mview: MView
+      mview: MView,
+      mview2: MView
     };
-    mixins = [HttpMixin];
+    mixins = [HttpMixin, ToastMixin];
     data = {
       items: [{
           name: "BUG反馈",
-          url: "/pages/my/feedback?index=0"
+          url: "/pages/my/feedback?index=0",
+          type: "page"
         },
         {
           name: "功能建议",
-          url: "/pages/my/feedback?index=1"
+          url: "/pages/my/feedback?index=1",
+          type: "page"
         },
         {
           name: "我的反馈",
-          url: "/pages/my/feedbackList"
+          url: "/pages/my/feedbackList",
+          type: "page"
         },
         {
           name: "账号绑定",
-          url: "/pages/bind"
+          url: "/pages/bind",
+          type: "page"
+        },
+        {
+          name: "QQ群: 698433701",
+          url: "698433701",
+          type: "copy"
         }
       ],
       nickName: "未授权",
@@ -134,9 +157,47 @@
     }
     methods = {
       to(item) {
-        wepy.navigateTo({
-          url: item.url
-        });
+        if (item.type == "copy") {
+          const self = this
+          wepy.setClipboardData({
+            data: item.url,
+            success: function(res) {
+              self.ShowToast("复制成功！")
+            },
+            fail: res => {
+              self.ShowToast("复制失败！")
+            }
+          })
+        } else {
+          wepy.navigateTo({
+            url: item.url
+          });
+        }
+      },
+      clear() {
+        const self = this
+        wepy.showModal({
+          title: "确认清空缓存",
+          content: "会删除所有本地数据，需要退出小程序后重新进入！",
+          success: function(res) {
+            if (res.confirm) {
+              try {
+                // 保留token 和 verify
+                const token = db.Get("token")
+                const verify = db.Get("verify")
+                wx.clearStorageSync()
+                db.Set("token", token)
+                db.Set("verify", verify)
+                // 关闭所有页面并且跳转到首页，确保verify数据重新获取
+                wepy.reLaunch({
+                  url: "/pages/index"
+                });
+              } catch (e) {
+                self.ShowToast("清空失败！")
+              }
+            }
+          }
+        })
       }
     };
     onLoad() {
