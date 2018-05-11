df1c85c9aa91e0042f9abaad31751202fd8c4919
e <1@lailin.xyz>
2018-05-11 10:25:13 +0800

    update

---
+++ src/pages/lostFind/lists.vue
@@ -0,0 +1,16 @@
+<style lang="less" scoped>
+
+</style>
+<template>
+  <view>
+    
+  </view>
+</template>
+<script>
+  import wepy from "wepy";
+  import HttpMixin from "mixins/http";
+  import ToastMixin from "mixins/toast";
+  import db from "util/db";
+  export default class BindJwc extends wepy.page {
+  }
+</script>
