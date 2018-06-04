7bc6c21fab9128ac5376d2a82fd563da90ea9da6
mohuishou <1@lailin.xyz>
2018-06-04 21:00:53 +0800

    新增图片校验

--- src/pages/lostFind/new.vue
+++ src/pages/lostFind/new.vue
@@ -9,7 +9,7 @@ page {
   margin-bottom: 20rpx;
   padding: 0 20rpx;
   label {
-    display: flex; 
+    display: flex;
     align-items: baseline;
     border-bottom: 2rpx solid #eee;
     &.desc {
@@ -69,19 +69,19 @@ button {
 
 <template>
   <view>
-    <form>
+    <form @submit="submit">
       <view class="group">
         <label>
           <view class="title">标题</view>
-          <input placeholder="请输入标题" />
+          <input name="title" placeholder="请输入标题" />
         </label>
         <label>
           <view class="title">地点</view>
-          <input placeholder="请输入地点" />
+          <input name="address" placeholder="请输入地点" />
         </label>
         <label>
           <view class="title">类型</view>
-          <picker @change="changeCategories" value="{{cid}}" range="{{categories}}">
+          <picker name="category" @change="changeCategories" value="{{cid}}" range="{{categories}}">
             <view class="picker">
               {{categories[cid]}}
             </view>
@@ -89,11 +89,11 @@ button {
         </label>
         <label class="desc">
           <view class="title">描述</view>
-          <textarea placeholder="请输入物品描述"></textarea>
+          <textarea name="info" placeholder="请输入物品描述"></textarea>
         </label>
         <label class="image">
           <view class="title">图片上传</view>
-          <input type="text" style="display: none;"  value="{{imageUrl}}" />
+          <input name="pictures" type="text" style="display: none;" value="{{imageUrl}}" />
           <view style="background-image: url({{imageUrl}});" @tap="chooseImg" class="image-up">
             <view wx:if="{{imageUrl == ''}}" class="iconfont icon-add"></view>
           </view>
@@ -102,11 +102,11 @@ button {
       <view class="group">
         <label>
           <view class="title">称呼</view>
-          <input placeholder="建议不要输入全名" />
+          <input name="user_name" placeholder="建议不要输入全名" />
         </label>
         <label>
           <view class="title">联系方式</view>
-          <picker @change="changeCallType" value="{{tid}}" range="{{callTypes}}">
+          <picker name="contact_type" @change="changeCallType" value="{{tid}}" range="{{callTypes}}">
             <view class="picker">
               {{callTypes[tid]}}
             </view>
@@ -114,10 +114,10 @@ button {
         </label>
         <label>
           <view class="title">联系</view>
-          <input placeholder="不建议使用手机号" />
+          <input name="contact" placeholder="{{tid == 4 ? '不建议使用手机号' : '请输入' + callTypes[tid]}}" />
         </label>
       </view>
-      <button @tap="submit" form-type="submit">提交课程评价</button>
+      <button form-type="submit">提交课程评价</button>
     </form>
   </view>
 </template>
@@ -126,48 +126,96 @@ import wepy from "wepy";
 import HttpMixin from "mixins/http";
 import ToastMixin from "mixins/toast";
 import db from "util/db";
-import {Upload} from "util/cos"
+import { Upload, ImageCheck } from "util/cos";
 export default class BindJwc extends wepy.page {
   config = {};
-  mixins = [HttpMixin, ToastMixin]
+  mixins = [HttpMixin, ToastMixin];
   data = {
     categories: ["报失", "一卡通招领", "其他招领"],
     cid: 0,
     callTypes: ["QQ", "微信", "微博", "邮箱", "手机"],
     tid: 0,
-    imageUrl: "",
+    imageUrl: ""
   };
   methods = {
-    changeCategories(e){
-      this.cid = e.detail.value || 0
+    changeCategories(e) {
+      this.cid = e.detail.value || 0;
     },
-    changeCallType(e){
-      this.tid = e.detail.value || 0
+    changeCallType(e) {
+      this.tid = e.detail.value || 0;
     },
-    chooseImg(){
-      const self = this
+    chooseImg() {
+      const self = this;
       wepy.chooseImage({
         count: 1, //最多可以选择的图片张数,
-        sizeType: ['compressed'],
+        sizeType: ["compressed"],
         success: res => {
-          let paths = res.tempFilePaths
-          if (!paths || paths.length == 0){
-            self.ShowToast("没有选取图片！")
-            return
+          let paths = res.tempFilePaths;
+          if (!paths || paths.length == 0) {
+            self.ShowToast("没有选取图片！");
+            return;
           }
-          self.imageUrl = paths[0]
-          self.$apply()
+          self.imageUrl = paths[0];
+          self.$apply();
         }, //返回图片的本地文件路径列表 tempFilePaths,
         fail: () => {
-          self.ShowToast("图片选取失败！")
+          self.ShowToast("图片选取失败！");
         },
         complete: () => {}
       });
     },
-    submit(e){
-      console.log(e);
-      Upload(this.imageUrl,"lost_find")
+    async submit(e) {
+      let params = e.detail.value;
+
+      // 参数校验
+      for (const key in params) {
+        if ((key != "image" && params[key] === "") || this.imageUrl === "") {
+          this.ShowToast("不能有空值！");
+          return;
+        }
+      }
+
+      // 图片检查
+      try {
+        wx.showLoading({
+          title: "图片检查中...",
+          mask: true
+        });
+        const check_res = await ImageCheck(this.imageUrl);
+      } catch (error) {
+        this.ShowToast(error);
+        return;
+      }
+
+      // 图片上传
+      try {
+        wx.hideLoading();
+        wx.showLoading({
+          title: "图片上传中...",
+          mask: true
+        });
+        const res = await Upload(this.imageUrl, "lost_find");
+        params.pictures = res.Location || "";
+      } catch (error) {
+        this.ShowToast("图片上传失败！");
+        return;
+      }
+      wx.hideLoading();
+
+      // 参数转换
+      params.contact = `[${this.callTypes[params.contact_type]}]: ${
+        params.contact
+      }`;
+      delete params.contact_type;
+      params.category = this.categories[params.category];
+
+      // 上传到服务器
+      try {
+        await this.PostWithBind("/lost_find", params);
+      } catch (error) {
+        console.log(error);
+      }
     }
-  }
+  };
 }
 </script>
--- src/util/cos.js
+++ src/util/cos.js
@@ -3,21 +3,59 @@ import {
   COSConf
 } from '../config'
 import dayjs from 'dayjs'
+import {
+  GET
+} from './http'
 
 // 初始化实例
 var cos = new COS(COSConf.key)
 
+/**
+ * 图片检查，调用小程序接口
+ * @param {string} filepath 图片路径
+ */
+export async function ImageCheck(filepath) {
+  const res = await GET('/wechat/token')
+  return new Promise((resolve, reject) => {
+    wx.uploadFile({
+      url: 'https://api.weixin.qq.com/wxa/img_sec_check?access_token=' + res.data,
+      filePath: filepath,
+      name: 'media',
+      success: resp => {
+        const data = JSON.parse(resp.data)
+        if (data === 0) {
+          resolve(true)
+        } else if (data === 87014) {
+          reject('图片包含违法违规内容')
+        } else {
+          resolve(data)
+        }
+      },
+      fail: err => {
+        reject(err)
+      }
+    })
+  })
+}
+
 export function Upload(filepath, prefix = '') {
   let filename = prefix + dayjs().format('/YYYY/M/D/') + new Date().getTime() + '.' + filepath.substr(filepath.lastIndexOf('.') + 1)
-  cos.postObject({
-    Bucket: COSConf.bucket,
-    Region: COSConf.region,
-    Key: filename,
-    FilePath: filepath,
-    onProgress: function (info) {
-      console.log(JSON.stringify(info))
-    }
-  }, function (err, data) {
-    console.log(err || data)
+
+  return new Promise((resolve, reject) => {
+    cos.postObject({
+      Bucket: COSConf.bucket,
+      Region: COSConf.region,
+      Key: filename,
+      FilePath: filepath,
+      onProgress: function (info) {
+        console.log(JSON.stringify(info))
+      }
+    }, function (err, data) {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data)
+      }
+    })
   })
 }
