ac25b1a9a0fcae6ce523180593902ed9c3aef000
mohuishou <1@lailin.xyz>
2018-06-08 00:32:55 +0800

    修复搜索按钮的bug

--- src/pages/course/search.vue
+++ src/pages/course/search.vue
@@ -1,74 +1,77 @@
 <style lang="less">
-  @import "./src/less/config";
-  page {
+@import "./src/less/config";
+page {
+  font-size: 28rpx;
+  width: 100%;
+  background: @bg-color;
+  > view {
+    width: 100%;
+  }
+}
+.search {
+  position: fixed; // top: 0;
+  padding: 10rpx 20rpx;
+  background: #fff;
+  width: 100%;
+  z-index: 9999;
+  display: flex;
+  justify-content: space-between;
+  height: 100rpx;
+  align-items: center;
+  border-bottom: 2rpx solid #ddd;
+  input {
+    width: calc(~"100% - 260rpx");
     font-size: 28rpx;
-    width: 100%;
-    background: @bg-color;
-    >view {
-      width: 100%;
-    }
+    padding: 10rpx;
+    padding-left: 20rpx;
+    height: 40rpx;
+    background: #f6f6f6;
   }
-  .search {
-    position: fixed; // top: 0;
-    padding: 10rpx 20rpx;
-    background: #fff;
-    width: 100%;
-    z-index: 9999;
-    display: flex;
-    justify-content: space-between;
-    height: 100rpx;
+  button {
+    border-radius: 0;
+    width: 150rpx;
+    height: 60rpx;
+    font-size: 28rpx;
+    display: inline-flex;
     align-items: center;
-    border-bottom: 2rpx solid #ddd;
-    input {
-      width: calc(~"100% - 260rpx");
-      font-size: 28rpx;
-      padding: 10rpx;
-      padding-left: 20rpx;
-      height: 40rpx;
-      background: #f6f6f6;
-    }
-    button {
-      border-radius: 0;
-      width: 150rpx;
-      height: 60rpx;
-      font-size: 28rpx;
-      display: inline-flex;
-      align-items: center;
-      justify-content: center;
-      background: linear-gradient(90deg, @base-color, #ed5a65);
-      color: #fff;
-      font-size: 28rpx;
-    }
-  }
-  .main {
-    z-index: 100;
-    padding-top: 120rpx;
-  }
-  .add {
-    width: 100%;
-    color: #888;
-    padding: 40rpx 0;
-    text-align: center;
-    font-size: 30rpx;
-  }
-  .add-schedule {
-    position: fixed;
-    bottom: 0;
+    justify-content: center;
     background: linear-gradient(90deg, @base-color, #ed5a65);
-    width: 100%;
     color: #fff;
-    text-align: center;
-    padding: 30rpx 0;
-    font-size: 30rpx;
+    font-size: 28rpx;
   }
+}
+.main {
+  z-index: 100;
+  padding-top: 120rpx;
+}
+.add {
+  width: 100%;
+  color: #888;
+  padding: 40rpx 0;
+  text-align: center;
+  font-size: 30rpx;
+}
+.add-schedule {
+  position: fixed;
+  bottom: 0;
+  background: linear-gradient(90deg, @base-color, #ed5a65);
+  width: 100%;
+  color: #fff;
+  text-align: center;
+  padding: 30rpx 0;
+  font-size: 30rpx;
+}
 </style>
 
 <template>
   <view>
-    <view class="search">
-      <input @confirm="search" @blur="changeSearch" name="name" placeholder="请输入课程名称" confirm-type="search" auto-focus/>
-      <button @tap="search">搜索</button>
-    </view>
+    <form @submit="submit">
+      <view class="search">
+        <input @confirm="search" @blur="changeSearch" name="name" placeholder="请输入课程名称" confirm-type="search" auto-focus/>
+        <button form-type="submit">搜索</button>
+      </view>
+    </form>
+
     <scroll-view style="height: {{height}}px;" scroll-y class="main" @scrolltolower="more">
       <repeat for="{{courses}}" item="data" key="index">
         <Card :item.sync="data"></Card>
@@ -84,94 +87,98 @@
 </template>
 
 <script>
-  import wepy from "wepy";
-  import HttpMixin from "mixins/http";
-  import ToastMixin from "mixins/toast";
-  import Card from "components/course/card"
-  const callTypes = ["", "不点名", "偶尔点名", "抽点", "全点"]
-  const examTypes = ["", "论文", "考试", "大作业", "其他"]
-  const taskTypes = ["", "没作业", "有作业"]
-  export default class CourseLists extends wepy.page {
-    config = {
-      navigationBarTitleText: '课程搜索',
-    };
-    mixins = [HttpMixin, ToastMixin];
-    components = {
-      Card: Card,
-    };
-    data = {
-      courses: [],
-      page: 1,
-      page_size: 15,
-      name: "",
-      height: 500,
-      from: "",
-      isEmpty: false,
+import wepy from "wepy";
+import HttpMixin from "mixins/http";
+import ToastMixin from "mixins/toast";
+import Card from "components/course/card";
+const callTypes = ["", "不点名", "偶尔点名", "抽点", "全点"];
+const examTypes = ["", "论文", "考试", "大作业", "其他"];
+const taskTypes = ["", "没作业", "有作业"];
+export default class CourseLists extends wepy.page {
+  config = {
+    navigationBarTitleText: "课程搜索"
+  };
+  mixins = [HttpMixin, ToastMixin];
+  components = {
+    Card: Card
+  };
+  data = {
+    courses: [],
+    page: 1,
+    page_size: 15,
+    name: "",
+    height: 500,
+    from: "",
+    isEmpty: false
+  };
+  newCourse(courses) {
+    for (let i = 0; i < courses.length; i++) {
+      const course = courses[i];
+      courses[i].call_name = callTypes[courses[i].call_name];
+      courses[i].task = taskTypes[courses[i].task];
+      courses[i].exam_type = examTypes[courses[i].exam_type];
+      courses[i].avg_grade = courses[i].avg_grade.toFixed(1);
+      courses[i].fail_rate = courses[i].fail_rate.toFixed(1);
+      let teachers = courses[i].teacher.split(",");
+      let is_more = teachers.length > 1 ? "等" : "";
+      courses[i].teacher = teachers[0] + is_more;
     }
-    newCourse(courses) {
-      for (let i = 0; i < courses.length; i++) {
-        const course = courses[i];
-        courses[i].call_name = callTypes[courses[i].call_name]
-        courses[i].task = taskTypes[courses[i].task]
-        courses[i].exam_type = examTypes[courses[i].exam_type]
-        courses[i].avg_grade = courses[i].avg_grade.toFixed(1)
-        courses[i].fail_rate = courses[i].fail_rate.toFixed(1)
-        let teachers = courses[i].teacher.split(",")
-        let is_more = teachers.length > 1 ? '等' : ''
-        courses[i].teacher = teachers[0] + is_more
-      }
-      return courses
-    }
-    async searchCourse() {
-      if (this.page > 1 && this.isEmpty) {
-        this.ShowToast("已经到底了！")
-        return
-      }
-      if (this.page == 1) {
-        this.isEmpty = false
-      }
-      const resp = await this.PostWithBind("/course/search", {
-        page: this.page,
-        page_size: this.page_size,
-        name: this.name,
-      })
-      if (this.page > 1) {
-        this.courses = this.courses.concat(this.newCourse(resp.data))
-      } else {
-        this.courses = this.newCourse(resp.data)
-      }
-      if (resp.data.length === 0) {
-        this.isEmpty = true
-      }
-      this.$apply()
-    }
-    methods = {
-      addCourse() {
-        // 跳转到自定义课程界面
-        wepy.navigateTo({
-          url: '/pages/addSchedule',
-        })
-      },
-      changeSearch(e) {
-        this.name = e.detail.value
-      },
-      more() {
-        this.page++;
-        this.searchCourse()
-      },
-      search(e) {
-        if (e.detail.value) {
-          this.name = e.detail.value
-        }
-        this.page = 1
-        this.searchCourse()
-      }
-    }
-    onLoad(option) {
-      if ('from' in option) {
-        this.from = option.from
-      }
-      this.height = wx.getSystemInfoSync().screenHeight - 61
-    }
+    return courses;
   }
+  async searchCourse() {
+    if (this.page > 1 && this.isEmpty) {
+      this.ShowToast("已经到底了！");
+      return;
+    }
+    if (this.page == 1) {
+      this.isEmpty = false;
+    }
+    const resp = await this.PostWithBind("/course/search", {
+      page: this.page,
+      page_size: this.page_size,
+      name: this.name
+    });
+    if (this.page > 1) {
+      this.courses = this.courses.concat(this.newCourse(resp.data));
+    } else {
+      this.courses = this.newCourse(resp.data);
+    }
+    if (resp.data.length === 0) {
+      this.isEmpty = true;
+      this.ShowToast("没有搜索到课程，请更换关键词");
+    }
+    this.$apply();
+  }
+  methods = {
+    addCourse() {
+      // 跳转到自定义课程界面
+      wepy.navigateTo({
+        url: "/pages/addSchedule"
+      });
+    },
+    changeSearch(e) {
+      this.name = e.detail.value;
+    },
+    more() {
+      this.page++;
+      this.searchCourse();
+    },
+    submit(e) {
+      this.name = e.detail.value.name;
+      this.page = 1;
+      this.searchCourse();
+    },
+    search(e) {
+      this.name = e.detail.value;
+      this.page = 1;
+      this.searchCourse();
+    }
+  };
+  onLoad(option) {
+    if ("from" in option) {
+      this.from = option.from;
+    }
+    this.height = wx.getSystemInfoSync().screenHeight - 61;
+  }
+}
 </script>
--- src/pages/library/search.vue
+++ src/pages/library/search.vue
@@ -1,82 +1,85 @@
 <style lang="less">
-  @import url("../../less/config");
-  page {
-    background: @bg-color;
+@import url("../../less/config");
+page {
+  background: @bg-color;
+}
+.search {
+  position: fixed; // top: 0;
+  padding: 10rpx 20rpx;
+  background: #fff;
+  width: 100%;
+  z-index: 9999;
+  display: flex;
+  justify-content: space-between;
+  height: 100rpx;
+  align-items: center;
+  border-bottom: 2rpx solid #ddd;
+  flex-wrap: nowrap;
+  input {
+    width: calc(~"100% - 260rpx");
+    font-size: 28rpx;
+    padding: 10rpx;
+    padding-left: 20rpx;
+    height: 40rpx;
+    background: #f6f6f6;
   }
-  .search {
-    position: fixed; // top: 0;
-    padding: 10rpx 20rpx;
-    background: #fff;
-    width: 100%;
-    z-index: 9999;
-    display: flex;
-    justify-content: space-between;
-    height: 100rpx;
+  button {
+    border-radius: 0;
+    width: 150rpx;
+    height: 60rpx;
+    font-size: 28rpx;
+    display: inline-flex;
     align-items: center;
-    border-bottom: 2rpx solid #ddd;
-    input {
-      width: calc(~"100% - 260rpx");
-      font-size: 28rpx;
-      padding: 10rpx;
-      padding-left: 20rpx;
-      height: 40rpx;
-      background: #f6f6f6;
-    }
-    button {
-      border-radius: 0;
-      width: 150rpx;
-      height: 60rpx;
-      font-size: 28rpx;
-      display: inline-flex;
-      align-items: center;
-      justify-content: center;
-      background: linear-gradient(90deg, @base-color, #ed5a65);
-      color: #fff;
-      font-size: 28rpx;
-    }
+    justify-content: center;
+    background: linear-gradient(90deg, @base-color, #ed5a65);
+    color: #fff;
+    font-size: 28rpx;
   }
-  .results {
-    width: 100%;
-    padding-top: 125rpx;
-    .result {
-      // margin-top: 0.5rem;
-      border-bottom: 3rpx solid #eee;
-      background: @white;
+}
+.results {
+  width: 100%;
+  padding-top: 125rpx;
+  .result {
+    // margin-top: 0.5rem;
+    border-bottom: 3rpx solid #eee;
+    background: @white;
+    display: flex;
+    padding: 0.5rem;
+    image {
+      width: 4.5rem;
+      height: 6rem;
+      background-image: url(https://scuplus.oss-cn-shenzhen.aliyuncs.com/book.jpg);
+      background-size: cover;
+      background-position: center;
+    }
+    .info {
+      width: calc(~"100% - 5.5rem");
+      align-content: space-between;
       display: flex;
-      padding: 0.5rem;
-      image {
-        width: 4.5rem;
-        height: 6rem;
-        background-image: url(https://scuplus.oss-cn-shenzhen.aliyuncs.com/book.jpg);
-        background-size: cover;
-        background-position: center;
-      }
-      .info {
-        width: calc(~"100% - 5.5rem");
-        align-content: space-between;
+      margin-left: 0.5rem;
+      flex-wrap: wrap;
+      .row {
+        width: 100%;
         display: flex;
-        margin-left: 0.5rem;
-        flex-wrap: wrap;
-        .row {
-          width: 100%;
-          display: flex;
-          justify-content: space-between;
-        }
-        .press {
-          font-size: 0.8rem;
-          color: #888;
-        }
+        justify-content: space-between;
+      }
+      .press {
+        font-size: 0.8rem;
+        color: #888;
       }
     }
   }
+}
 </style>
 
 <template>
   <view>
-    <view class="search">
-      <input @blur="change" @confirm="search" name="name" placeholder="请输入图书名称" confirm-type="search" auto-focus/>
-      <button @tap="search">搜索</button>
-    </view>
+    <form @submit="submit">
+      <view class="search">
+        <input @blur="change" @confirm="search" name="name" placeholder="请输入图书名称" confirm-type="search" auto-focus/>
+        <button form-type="submit">搜索</button>
+      </view>
+    </form>
     <scroll-view style="height:{{height}}px;" class="results" @scrolltolower="next" enable-back-to-top scroll-y>
       <block wx:for="{{books}}" wx:key="index">
         <view @tap="show({{item}})" class="result">
@@ -97,84 +100,88 @@
 </template>
 
 <script>
-  import wepy from 'wepy'
-  import HttpMixin from "../../mixins/http";
-  import ToastMixin from "../../mixins/toast";
-  export default class Search extends wepy.page {
-    mixins = [HttpMixin, ToastMixin]
-    components = {}
-    config = {
-      navigationBarTitleText: '图书搜索'
-    };
-    data = {
-      books: [],
-      next_page: '',
-      height: 280,
-      loading: false,
-      name: "",
-    };
-    methods = {
-      search(e) {
-        if (e.detail.value) {
-          this.name = e.detail.value
-        }
-        this.getBooks(this.name, false)
-      },
-      change(e) {
-        this.name = e.detail.value
-      },
-      next(e) {
-        this.getBooks('', true)
-      },
-      show(item) {
-        let content = `索书号: ${item.number};\r\n`
-        item.book_addresses.forEach(e => {
-          content += `${e.address}: 馆藏 ${e.all_count},可借 ${e.loan_count};\r\n`
-        })
-        wepy.showModal({
-          title: item.title,
-          content: content,
-          showCancel: false
-        })
-      }
-    };
-    async getBooks(keyword, isNext) {
-      let nextPage = ''
-      if (this.loading) return
-      if (isNext) {
-        nextPage = this.next_page
-        if (nextPage === '') {
-          this.ShowToast('已经到底了')
-          return
-        }
-      }
-      this.loading = true
-      const resp = await this.POST('/library/search', {
-        keyword: keyword,
-        key_type: 'WRD',
-        next_page: nextPage
-      })
-      this.loading = false
-      if (resp.data.books.length === 0) {
-        console.log("sadadas");
-        this.ShowToast('没有搜索到图书')
-        return
-      }
-      if (nextPage === '') {
-        this.books = resp.data.books
-      } else {
-        this.books = this.books.concat(resp.data.books)
-      }
-      this.next_page = resp.data.next_url
-      this.$apply()
+import wepy from "wepy";
+import HttpMixin from "../../mixins/http";
+import ToastMixin from "../../mixins/toast";
+export default class Search extends wepy.page {
+  mixins = [HttpMixin, ToastMixin];
+  components = {};
+  config = {
+    navigationBarTitleText: "图书搜索"
+  };
+  data = {
+    books: [],
+    next_page: "",
+    height: 280,
+    loading: false,
+    name: ""
+  };
+  methods = {
+    submit(e) {
+      this.name = e.detail.value.name;
+      this.getBooks(this.name, false);
+    },
+    search(e) {
+      this.name = e.detail.value;
+      this.getBooks(this.name, false);
+    },
+    change(e) {
+      this.name = e.detail.value;
+    },
+    next(e) {
+      this.getBooks("", true);
+    },
+    show(item) {
+      let content = `索书号: ${item.number};\r\n`;
+      item.book_addresses.forEach(e => {
+        content += `${e.address}: 馆藏 ${e.all_count},可借 ${
+          e.loan_count
+        };\r\n`;
+      });
+      wepy.showModal({
+        title: item.title,
+        content: content,
+        showCancel: false
+      });
     }
-    onLoad() {
-      this.height = wx.getSystemInfoSync().windowHeight - 36
-    }
-    onShareAppMessage(options) {
-      return {
-        title: '搜索书籍'
+  };
+  async getBooks(keyword, isNext) {
+    let nextPage = "";
+    if (this.loading) return;
+    if (isNext) {
+      nextPage = this.next_page;
+      if (nextPage === "") {
+        this.ShowToast("已经到底了");
+        return;
       }
     }
+    this.loading = true;
+    const resp = await this.POST("/library/search", {
+      keyword: keyword,
+      key_type: "WRD",
+      next_page: nextPage
+    });
+    this.loading = false;
+    if (resp.data.books.length === 0) {
+      console.log("sadadas");
+      this.ShowToast("没有搜索到图书");
+      return;
+    }
+    if (nextPage === "") {
+      this.books = resp.data.books;
+    } else {
+      this.books = this.books.concat(resp.data.books);
+    }
+    this.next_page = resp.data.next_url;
+    this.$apply();
   }
+  onLoad() {
+    this.height = wx.getSystemInfoSync().windowHeight - 36;
+  }
+  onShareAppMessage(options) {
+    return {
+      title: "搜索书籍"
+    };
+  }
+}
 </script>
